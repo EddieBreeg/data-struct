595aec2baede0fd00e2dcc949a8c8cff08c1ffa2
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-07 23:24:41 +0200

    init dev branch

--- dictEdit.py
+++ dictEdit.py
@@ -3,7 +3,6 @@ def modifyDict(dict, path):
         dict[path[0]]=path[1]
         if path[1]==None:
             del dict[path[0]]
-        print(dict)
         return dict
     dict.setdefault(path[0], {})
     dict[path[0]]=modifyDict(dict[path[0]], path[1:])
