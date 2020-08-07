dce40182aeed03e707fbfdea592c1b4680b560a8
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-08 01:27:10 +0200

    added isValuein and pathTo functions

--- dictEdit.py
+++ dictEdit.py
@@ -1,13 +1,40 @@
-def modifyDict(dict, path):
+def modifyStruct(struct, path):
     if len(path)==2:
-        dict[path[0]]=path[1]
+        struct[path[0]]=path[1]
         if path[1]==None:
-            del dict[path[0]]
-        return dict
-    dict.setdefault(path[0], {})
-    dict[path[0]]=modifyDict(dict[path[0]], path[1:])
-    if dict[path[0]]=={}:
-        del dict[path[0]]
-    return dict
+            del struct[path[0]]
+        return struct
+    struct.setdefault(path[0], {})
+    struct[path[0]]= modifyStruct(struct[path[0]], path[1:])
+    if struct[path[0]]=={}:
+        del struct[path[0]]
+    return struct
 
+def isValueIn(struct, value):
+    if struct==value:
+        return True
+
+    elif type(struct)==list:
+        for x in struct:
+            if isValueIn(x, value):
+                return True
+        return False
+    elif type(struct)==dict:
+        for k in struct:
+            if isValueIn(struct[k], value):
+                return True
+    return False
+
+def pathTo(struct, value):
+    if struct==value:
+        return [value]
+
+    if type(struct)==dict:
+        sequence=struct.keys()
+    else:
+        sequence=range(len(struct))
+
+    for k in sequence:
+        if isValueIn((sub := struct[k]), value):
+            return [k] + pathTo(sub, value)
 
