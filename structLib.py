4e8f9695bc16de60681c9d9e0e0d16840470c27d
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-08 16:50:28 +0200

    updated Struct methods

--- structLib.py
+++ structLib.py
@@ -20,16 +20,35 @@ class Struct:
         modifyStruct(self.data, list(path)+[value])
     def __delitem__(self, path):
         self[path]=None
-    def __contains__(self, item):
-        return isValueIn(self.data, item)
+    def __contains__(self, key):
+        return isKeyIn(self.data, key)
     def __getitem__(self, path):
         return getItem(self.data, list(path))
+    def __iter__(self):
+        return self.data.__iter__()
+    def isValueIn(self, value):
+        return isValueIn(self.data, value)
+    def pathToValue(self, value):
+        return pathToValue(self.data, value)
+    def getAll(self, key):
+        return getAll(self.data, key)
+    def replace(self, old, new):
+        result=copy.deepcopy(self)
+        if (path:=result.pathToValue(old))!=None:
+            del path[-1]
+            result[path]=new
+        return result
+    def replaceAll(self, old, new):
+        result = copy.deepcopy(self)
+        while (path := result.pathToValue(old)) != None:
+            del path[-1]
+            result[path] = new
+        return result
 
 def getItem(data, path):
     if path==[]:
         return data
     return getItem(data[path[0]], path[1:])
-
 
 def modifyStruct(data, path):
     if len(path) == 2:
@@ -43,7 +62,6 @@ def modifyStruct(data, path):
     if data[path[0]] == {}:
         del data[path[0]]
     return data
-
 
 def isValueIn(data, value):
     if data == value:
@@ -74,7 +92,6 @@ def pathToValue(data, value):
         if isValueIn((sub := data[k]), value):
             return [k] + pathToValue(sub, value)
 
-
 def isKeyIn(data, key):
     sequence = []
     if type(data) == list:
@@ -101,6 +118,3 @@ def getAll(data, key):
         else:
             values += getAll(data[k], key)
     return values
-
-
-# def deleteValue(struct, value)
