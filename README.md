287a3c1d3936921f4e79d5f29ee147fed9253f2a
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-09 02:03:41 +0200

    pip packaging

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # structLib
-This package contains the _Struct_ class which is specifically
-aimed at making your life easier when you are handling JSON-like 
-objects (meaning nested _dict_ / _list_ objects usually)
+
+structLib is essentially a module which contains the _Struct_ class. This class is specifically
+aimed at making your life easier when working with JSON-like objects (meaning nested
+_dict_ / _list_ objects)
+
+## Installation
+
+To install structLib on your python setup, run the following:
+```python
+pip install structLib
+```
+On linux or mac you will probably need to run this command instead:
+```python
+python3 -m pip install structLib
+```
+Please note that version 3.8 (or higher) of python is required to run this library!
--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="structLib-Eddie-Breeg",
+    name="structLib",
     version="0.0.1",
     author="Eddie Breeg",
-    author_email="eddiebreeg0@protonmail.com",
-    description="A class to handle complex JSON-like data objects more easily",
+    description="A class to help you",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/EddieBreeg/structLib",
-    packages=setuptools.find_packages(),
+    url="https://github.com/pypa/sampleproject",
+    py_modules=['structLib'],
+    package_dir={'': "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
