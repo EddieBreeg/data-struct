0a1eef8f6c19f306021e8b4e4734097acb2b94fa
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-10 15:00:20 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="structLib-EddieBreeg",
-    version="0.2.0",
+    version="0.2.1",
     author="Eddie Breeg",
     license="GPLv3",
     author_email="eddiebreeg0@protonmail.com",
