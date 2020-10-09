eb3808f83a0a124bb59902206feee669d7772cbe
EddieBreeg <eddiebreeg0@protonmail.com>
2020-10-09 18:55:55 +0200

    1.2.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="data-struct",
-    version="1.1.0",
+    version="1.2.0",
     author="Eddie Breeg",
     license="GPLv3",
     author_email="eddiebreeg0@protonmail.com",
